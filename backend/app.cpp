83b83cdd4ed3737f512192bc019ea61949c2a20a
Gamini_Sai_Lakshmi_Anjani <gaminianjani@gmail.com>
2026-02-26 15:11:02 +0530

    Fixed backend server to run continuously

--- backend/app.cpp
+++ backend/app.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
-using namespace std;
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 int main() {
-    cout << "Hello from Backend Container!" << endl;
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[30000] = {0};
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    std::string response =
+        "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nServed by backend container\n";
+
+    while (true) {
+        new_socket = accept(server_fd, (struct sockaddr *)&address,
+                            (socklen_t*)&addrlen);
+        read(new_socket, buffer, 30000);
+        send(new_socket, response.c_str(), response.length(), 0);
+        close(new_socket);
+    }
+
     return 0;
 }
