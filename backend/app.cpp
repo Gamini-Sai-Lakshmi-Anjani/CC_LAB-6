56a9b84bffd6ca1e14327f8b0573c2baa7680d7c
Gamini_Sai_Lakshmi_Anjani <gaminianjani@gmail.com>
2026-02-26 15:48:51 +0530

    Updated backend to show hostname

--- backend/app.cpp
+++ backend/app.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 
 int main() {
     int server_fd, new_socket;
@@ -9,22 +10,51 @@ int main() {
     int addrlen = sizeof(address);
     char buffer[30000] = {0};
 
+    // Create socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0) {
+        perror("Socket failed");
+        return 1;
+    }
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
 
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    listen(server_fd, 3);
+    // Bind socket
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        return 1;
+    }
 
-    std::string response =
-        "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nServed by backend container\n";
+    // Listen
+    if (listen(server_fd, 10) < 0) {
+        perror("Listen failed");
+        return 1;
+    }
+
+    std::cout << "Backend server running on port 8080..." << std::endl;
 
     while (true) {
         new_socket = accept(server_fd, (struct sockaddr *)&address,
                             (socklen_t*)&addrlen);
+
+        if (new_socket < 0) {
+            perror("Accept failed");
+            continue;
+        }
+
         read(new_socket, buffer, 30000);
+
+        // Get container hostname
+        char hostname[1024];
+        gethostname(hostname, sizeof(hostname));
+
+        std::string response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n\r\n"
+            "Served by: " + std::string(hostname) + "\n";
+
         send(new_socket, response.c_str(), response.length(), 0);
         close(new_socket);
     }
